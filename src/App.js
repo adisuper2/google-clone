bc4b687e5b1452dbed40c6af23aab71a4e4c63e9
1905586 <adisuper2>
2022-06-29 10:43:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import SearchPage from "./pages/SearchPage"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY="AIzaSyClDE_L784cEthMdTfTh7CAo7RQTRMY52M"
+export default API_KEY
